4e36f8d96e028adbb3a2b0d998d46ead95791ee7
CodeFikz <fikilembiza63@gmail.com>
2024-02-02 16:24:38 -0800

    python test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+!/usr/bin/python3
+"""
+a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b
+You are not allowed to import any module
+"""
+
+
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
